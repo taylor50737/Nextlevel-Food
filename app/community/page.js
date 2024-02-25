ac4b14385669792e981610970d52e7ef3a9ed7f9
Taylor Lo <taylorloph@gmail.com>
2024-02-25 11:37:51 -0500

    Setup page routes

---
+++ app/community/page.js
@@ -0,0 +1,3 @@
+export default function CommunityPage() {
+  return <h1>Community Page</h1>;
+}
---
+++ app/meals/page.js
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return <h1>Meals Page</h1>;
+}
